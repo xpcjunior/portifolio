9628d128cc833fbeb6fd0337420bfc566a05895d
Paulo Cabral <paulo.junior.cabral@gmail.com>
2023-11-10 11:59:41 -0300

    Ajustada mensagem do rodapé, atualizado favicon e corrigido erro de português na palavra portfólio

--- src/app/shared/modules/fa-icons.module.ts
+++ src/app/shared/modules/fa-icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngular, faGithub, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faCopyright, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faCode, faMoon, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -15,6 +15,7 @@ export class FontAwesomeIconsModule {
     library.addIcons(
       faAngular,
       faCode,
+      faCopyright,
       faEnvelope,
       faGithub,
       faLinkedin,
